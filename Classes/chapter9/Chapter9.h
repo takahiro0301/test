092aa983342a6b6219e49e90c149d312213642c6
takahiro0301 <jojoimahama@gmail.com>
2016-02-24 22:27:08 +0900

    add Classes and Resources

---
+++ Classes/chapter9/Chapter9.h
@@ -0,0 +1,22 @@
+#ifndef __CHAPTER9_H__
+#define __CHAPTER9_H__
+
+#include "cocos2d.h"
+
+class Chapter9 : public cocos2d::Node
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+    
+    // a selector callback
+    void onMenuCloseCallback(cocos2d::Ref* sender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(Chapter9);
+};
+
+#endif // __CHAPTER9_H__
